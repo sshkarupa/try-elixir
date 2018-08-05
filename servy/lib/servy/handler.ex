0f9426d0986bb71c6d2db97ac224942c14966a3e
Sergey Shkarupa <s.shkarupa@gmail.com>
2018-08-05 13:11:48 +0200

    Add module attributes

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,4 +1,9 @@
 defmodule Servy.Handler do
+  @moduledoc "Handles HTTP requests"
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transforns the request into a response"
   def handle(request) do
     request
     |> parse
@@ -35,8 +40,9 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
+
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read
     |> handle_file(conv)
