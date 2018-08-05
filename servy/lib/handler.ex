fb5f1abed24bf18c55fea6515b72b2b290bbada1
Sergey Shkarupa <s.shkarupa@gmail.com>
2018-08-05 11:04:50 +0200

    feat: Add log finction inside pipeline

--- servy/lib/handler.ex
+++ servy/lib/handler.ex
@@ -2,6 +2,7 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
@@ -15,6 +16,8 @@ defmodule Servy.Handler do
 
     %{method: method, path: path, resp_body: ""}
   end
+
+  def log(conv), do: IO.inspect conv
 
   def route(conv) do
     %{conv | resp_body: "Bears, Lions, Tigers"}
