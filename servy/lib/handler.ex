4d03b870f6f0bfade00dc1d5a71bf24e9ff87473
Sergey Shkarupa <s.shkarupa@gmail.com>
2018-08-05 11:13:51 +0200

    feat: Add clauses for different routes

--- servy/lib/handler.ex
+++ servy/lib/handler.ex
@@ -20,7 +20,19 @@ defmodule Servy.Handler do
   def log(conv), do: IO.inspect conv
 
   def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
     %{conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(conv, _method, path) do
+    %{conv | resp_body: "No route #{conv.path} here!"}
   end
 
   def format_response(conv) do
@@ -36,6 +48,31 @@ end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+
+request = """
+GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
